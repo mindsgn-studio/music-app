cfae2a260e1a3652e91e50831a63747514aeae56
mindsgn <seni.tembe@protonmail.com>
2024-01-05 15:25:41 +0200

    update

--- src/screen/home/index.tsx
+++ src/screen/home/index.tsx
@@ -2,17 +2,18 @@ import React from 'react';
 import {ScrollView} from 'react-native';
 import styles from './style';
 import {AlbumCard} from '../../components';
+import Realm from 'realm';
 
 const Home = (props: any) => {
+  const realm = Realm();
   const {navigation} = props;
 
-  console.log(props.navigation.navigation);
-  const goToAlbum = () => {
-    navigation.navigate('Album');
+  const goToAlbum = (album: string) => {
+    navigation.navigate('Album', {});
   };
 
-  const goToArtist = () => {
-    navigation.navigate('Artist');
+  const goToArtist = (artist: string) => {
+    navigation.navigate('Artist', {});
   };
 
   return (
